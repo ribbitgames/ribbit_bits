a7ffc8b7389f17f2c6b2d0aecb1d896c57385164
Frédéric Joanis <frederic.joanis@gmail.com>
2025-02-07 18:41:57 -0500

    Bit renamed

--- src/bit_name.rs
+++ src/bit_name.rs
@@ -3,17 +3,16 @@ use strum::{Display, EnumCount, EnumString, FromRepr};
 #[repr(C)]
 #[derive(Debug, Copy, Clone, EnumString, EnumCount, FromRepr, Display, PartialEq, Eq)]
 pub enum BitName {
+    EmojiGrabber,
     FlappyGun,
     MasterMind,
     MathQuiz,
-    //Maze,
     Puzzle15,
-    ShapeFinder,
     ShapeGrabber,
     ShapeMasher,
     ShapeMemorizer,
     ShapeRacer,
-    ShapeSidescroller,
     ShapeShooter,
+    ShapeSidescroller,
     WheresWaldo,
 }
